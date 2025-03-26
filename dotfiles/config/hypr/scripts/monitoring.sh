f047c08637c2244fd5d62f2233a716011282aa79
mStenz <michael.stenz2006@gmail.com>
2025-03-26 12:53:17 +0100

    100% battery border color

--- dotfiles/config/hypr/scripts/monitoring.sh
+++ dotfiles/config/hypr/scripts/monitoring.sh
@@ -16,21 +16,28 @@ COLOR_TERTIARY=$(get_color "color_tertiary")
 COLOR_LOW_BATTERY=$(get_color "color_low_battery")
 COLOR_LOW_BATTERY_CHARGING=$(get_color "color_low_battery_charging")
 COLOR_CHARGING=$(get_color "color_charging")
+COLOR_BATTERY_FULL=$(get_color "color_battery_full")
+
+setBorderColor() {
+    hyprctl keyword general:col.active_border "rgba(${1})"
+}
 
 updateCharging() {
     curr_charge=$(cat /sys/class/power_supply/BAT0/charge_now)
     charging_status=$(cat /sys/class/power_supply/BAT0/status)
-    if test $curr_charge -lt $charge_alarm_threshold; then
+    if [ "$curr_charge" = "$max_charge" ]; then
+	setBorderColor "$COLOR_BATTERY_FULL"
+    elif test $curr_charge -lt $charge_alarm_threshold; then
 	if [ "$charging_status" = "Charging" ]; then
-            hyprctl keyword general:col.active_border "rgba(${COLOR_LOW_BATTERY_CHARGING})"
+            setBorderColor "$COLOR_LOW_BATTERY_CHARGING"
 	else
-            hyprctl keyword general:col.active_border "rgba(${COLOR_LOW_BATTERY})"
+            setBorderColor "$COLOR_LOW_BATTERY"
 	fi
     else
 	if [ "$charging_status" = "Charging" ]; then
-            hyprctl keyword general:col.active_border "rgba(${COLOR_CHARGING})"
+            setBorderColor "$COLOR_CHARGING"
 	else
-            hyprctl keyword general:col.active_border "rgba(${COLOR_PRIMARY})"
+            setBorderColor "$COLOR_PRIMARY"
 	fi
     fi
 }
@@ -39,4 +46,3 @@ while true; do
     updateCharging "$curr_charge"
     sleep "$sleepTimer"
 done
-
